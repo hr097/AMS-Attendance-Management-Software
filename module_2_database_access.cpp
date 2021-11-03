9b46cb3112a1353c4b1b7e42c32092e71a2c7f66
DrashtiDhola2109 <83170084+DrashtiDhola2109@users.noreply.github.com>
2021-11-03 14:50:29 +0530

    data extraction for getting the folder key is completed

--- module_2_database_access.cpp
+++ module_2_database_access.cpp
@@ -1,82 +1,151 @@
 #include<iostream>
-#include<string>
+#include<string> 
 #include<fstream>
 #include<vector>
 #include<algorithm>
-#include<stdio.h>
+#include<stdlib.h>
+#include<tuple>    //* for using tuples (group of element key->value type)
 
 using namespace std;
 
+int checkDuplicate(vector<string> vec,string search) //*for cheking if duplicate records found in vector_storage
+{
+    vector<string>::iterator it; //iterator
+
+    it = find(vec.begin(), vec.end(),search); // finding elemnt in vector
+
+    if (it != vec.end())
+	{
+      return 0; // if found then return 0
+	}
+	else
+    {
+      return 1; // if not found then return 1
+    }
+}
+
 int main()
 {
-    // int i,j,total;
-    vector<string> buffer;
-    fstream fin("sample.txt",ios::in);
-    // char data[15][50], newdata[50];
+    vector<string> buffer,LIST; //vector buffer for file handling data receiver
 
-    string line,course_name,sem,subject,command;
-    // fin.open; //*in -->reading mode
-    if(!fin.is_open())
+    string TEMP_STR[4];//course name , sem , sub , folder_accces_path
+    
+    vector<tuple<string,string,string,string>> DATA; // search-access vector-tuple
+
+    fstream fin("sample.txt",ios::in); // file opened in reading mode
+    
+    if(!fin.is_open()) // if not opened
     {
         cout<<" FILE NOT OPENED !";
     }
-    else
+    else // if opened
     {
-        getline(fin,line);
+        getline(fin,TEMP_STR[1]); // TEMP_STR[1] used as temporary storage
         
-        while(!fin.eof())
+        while(!fin.eof()) // data receive until file ends
         {    
-            buffer.push_back(line);
-            getline(fin,line);
+            buffer.push_back(TEMP_STR[1]); // save that string(data) in vector
+            getline(fin,TEMP_STR[1]); // fetch again from file
         }  
     }
-    
-    cout<<endl<<"bufferdata"<<endl;
 
-    for (auto i = buffer.begin(); i != buffer.end(); ++i)
-        cout << *i<<endl;
+    sort(buffer.begin(), buffer.end()); //sorted file data in vector in Dictionary order
 
-    sort(buffer.begin(), buffer.end());
-    string displayData[buffer.size()][3];
-
-    //cout<<buffer.size()<<endl;
-    //  for (auto i = buffer.begin(); i != buffer.end(); ++i)  
-        //  cout << *i<<endl;
-        // 
-
-    command = course_name + "-" + sem + "-" + subject;
-    cout<< endl << command;
-    
-    int j=0;
-
-    for (auto i = buffer.begin(); i != buffer.end(); ++i,++j)
+    for (auto i = buffer.begin(); i != buffer.end(); ++i) //insert in to vector-tuple from vector buffer with extracting string
     {
-        string data = (*i);
-        //cout<<endl<<"data : "<<data<<endl;
-        int found_pos = data.find("|");
+        string data = (*i); // temp variable for moving vector data to string
+        int found_pos,temp_pos; // temp variable for position storing
+
+        found_pos = data.find("|"); // find the first occurance of '|'
+        TEMP_STR[0] = data.substr(0,(found_pos)); // get proper course name from string and save it to TEMP_STR[0]
+        temp_pos = (found_pos+1); // making next finding position
+
+         if(checkDuplicate(LIST,TEMP_STR[0])) // cheking if duplicate 
+         {
+             LIST.push_back(TEMP_STR[0]); //making list for input of UI screen
+         }
         
-        displayData[j][0] = data.substr(0,(found_pos));
-        int temp_pos = (found_pos+1);
-        
-        // |1|
-        // AMS|10|AMS_S
+        found_pos = data.find("|",temp_pos);     //finds string till 1st pipe i.e coursename
+        TEMP_STR[1] = data.substr(temp_pos,(found_pos-temp_pos));//stores the string in TEMP_STR
+        temp_pos = (found_pos+1);  //searching next position
 
-        found_pos = data.find("|",temp_pos);
-        displayData[j][1] = data.substr(temp_pos,(found_pos-temp_pos));
-        temp_pos = (found_pos+1);
+        found_pos = data.find("|", temp_pos);   //finds string till 2nd pipe i.e. semester
+        TEMP_STR[2] = data.substr(temp_pos,(found_pos-temp_pos)); //stores the string in TEMP_STR
+        temp_pos = (found_pos+1);  //searching next position
+         
+        found_pos = data.find("|", temp_pos); //finds string till 3rd pipe i.e. subjectname
+        TEMP_STR[3] = data.substr(temp_pos); //stores the string in TEMP_STR
 
-        found_pos = data.find("|", temp_pos);
-        displayData[j][2] = data.substr(temp_pos,(found_pos-temp_pos));
-        temp_pos = (found_pos+1);
-
-        //found_pos = data.find("|", temp_pos);
-        displayData[j][3] = data.substr(temp_pos);
-
-        cout<<endl<<displayData[j][0]<<"\t"<<displayData[j][1]<<"\t"<<displayData[j][2]<<"\t"<<displayData[j][3];   
-
+        DATA.push_back(make_tuple(TEMP_STR[0],TEMP_STR[1],TEMP_STR[2],TEMP_STR[3]) ); //makes a final string to display
     }
+
+    for (auto i =LIST.begin(); i !=LIST.end(); ++i)
+    cout<<endl<< *i;; //displaying final course list
+
+    LIST.clear();//flush vector data for re-using
+
+    cout<<endl<<"choose course : ";
+    cin>>TEMP_STR[0];
+
+    for (auto i =DATA.begin(); i != DATA.end();++i)  //process to find searched data by user
+     {   
+       
+         if(TEMP_STR[0]==get<0>((*i))) //! input required
+         {   
+            if(checkDuplicate(LIST,get<1>((*i)))) // cheking if duplicate 
+            {
+                LIST.push_back(get<1>((*i))); //making list for input of UI screen
+            }
+         }
+                
+     }
+     
+    for (auto i =LIST.begin(); i !=LIST.end(); ++i)
+    cout<<endl<< *i;; //displaying final course list
+
+    LIST.clear();//flush vector data for re-using
     
+    cout<<endl<<"choose semester : ";
+    cin>>TEMP_STR[1];
 
 
-     return 0;
+    for (auto i =DATA.begin(); i != DATA.end();++i)  //process to find searched data by user
+     {   
+       
+         if(TEMP_STR[0]==get<0>((*i))&&TEMP_STR[1]==get<1>((*i))) //! input required
+         {
+            if(checkDuplicate(LIST,get<2>((*i)))) // cheking if duplicate 
+            {
+                LIST.push_back(get<2>((*i))); //making list for input of UI screen
+            }
+         }
+                
+     }
+
+    for (auto i =LIST.begin(); i !=LIST.end(); ++i)
+    cout<<endl<< *i;; //displaying final subjectlist
+
+    LIST.clear();//flush vector data for re-using
+
+    cout<<endl<<"choose subject : ";
+    cin>>TEMP_STR[2];
+     
+
+    cout<<endl<<"searched data :"<<endl;
+
+    TEMP_STR[3]="NOT FOUND !";// set temp_str = not found flag so if data not found then print it
+
+    for (auto i =DATA.begin(); i != DATA.end();++i)  //process to find searched data by user
+    {   
+      
+        if(TEMP_STR[0]==get<0>((*i)))
+            if(TEMP_STR[1]==get<1>((*i)))
+                if(TEMP_STR[2]==get<2>((*i)))
+                      TEMP_STR[3]=get<3>((*i)); 
+      
+    }
+
+    cout<<endl<<TEMP_STR[3]; 
+
+    return 0;
 }
