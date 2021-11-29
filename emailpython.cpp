109a427afc7dd685bd4258248f979a4a5b55f5e9
Harshil Ramani <harshilramani9777@gmail.com>
2021-11-30 00:24:07 +0530

    final edit

--- emailpython.cpp
+++ emailpython.cpp
@@ -3,12 +3,14 @@
 #include<conio.h>
 #include<stdbool.h> // * boolean operation
 
+//*NOTE THAT : ACCORDING TO REQUIREMENT sendToEmail() can be changed and you must have only python installed on your system in order to send email and run this function
+bool email_flag;
+bool process_flag;
+
 using namespace std;
 
-  bool sendToEmail(string SenderAddress,string password,string RecepientAddress,string Subject,string AttachmentPath,string Msg="") //? sending email to user
+   void sendToEmail(string SenderAddress,string password,string RecepientAddress,string Subject,string Msg,string AttachmentPath,string fileName) //? sending email to user
   {   
-      
-     
       command.clear(); // clear for re-using
   
       command = AMS_Path + "\\OTHER\\mail.py"; // make database path for email.py
@@ -20,7 +22,6 @@ using namespace std;
           cout << "DATA BASE-ERROR-201-204! ";//error 
           scrClr(2);
           exit(1);
-          return(false);
       }
       else //CREATE EMAIL PYTHON FILE
       {   
@@ -28,13 +29,13 @@ using namespace std;
   
           //* make python code for email
   
-          command ="import smtplib\nfrom email.mime.multipart import MIMEMultipart\nfrom email.mime.text import MIMEText\nfrom email.mime.base import MIMEBase\nfrom email import encoders\nfromaddr = \"";
-          command = command + SenderAddress + "\"\ntoaddr = \"" + RecepientAddress + "\"\n"; //"ams.software.team@gmail.com"  
+          command ="import smtplib\nfrom email.mime.multipart import MIMEMultipart\nfrom email.mime.text import MIMEText\nfrom email.mime.base import MIMEBase\nfrom email import encoders\nimport sys\nclass DevNull:\n    def write(self, msg):\n        pass\n\nsys.stderr = DevNull()\nfromaddr = \"";
+          command = command + SenderAddress + "\"\ntoaddr = \"" + RecepientAddress + "\"\n"; 
           command = command + "msg = MIMEMultipart()\nmsg['From'] = fromaddr\nmsg['To'] = toaddr\nmsg['Subject'] = \"";
-          command = command + Subject +"\"\nbody = \"" + Msg + "\"\nmsg.attach(MIMEText(body, 'plain'))\n";
+          command = command + Subject +"\"\nbody = \"\"\"" + Msg + "\"\"\"\nmsg.attach(MIMEText(body, 'plain'))\n";
           command = command + "filename = r\"" + AttachmentPath + "\"\n";    
           command = command + "attachment = open(filename,\"rb\")" + "\n";
-          command = command + "part = MIMEBase('application', 'octet-stream')\npart.set_payload((attachment).read())\nencoders.encode_base64(part)\npart.add_header('Content-Disposition', \"attachment; filename= \%s\" \% filename)\nmsg.attach(part)\nserver = smtplib.SMTP('smtp.gmail.com', 587)\nserver.starttls() \n";
+          command = command + "part = MIMEBase('application', 'octet-stream')\npart.set_payload((attachment).read())\nencoders.encode_base64(part)\npart.add_header('Content-Disposition', \"attachment; filename= \%s\" \% \" " + fileName + "\")\nmsg.attach(part)\nserver = smtplib.SMTP('smtp.gmail.com', 587)\nserver.starttls() \n";
           command = command +"server.login(fromaddr, \""+ password + "\")\n";
           command = command +"text = msg.as_string()\nserver.sendmail(fromaddr, toaddr, text)\nserver.quit()\n";              
   
@@ -42,9 +43,9 @@ using namespace std;
           write.close();// file closed
           
           command.clear();
-          command = "python "+AMS_Path + "\\OTHER\\mail.py"; 
+          command = "python "+AMS_Path + "\\OTHER\\mail.py "; 
   
-          system(command.c_str()); //* FILE SENDING TO EMAIL USING PYTHON CODE
+          int err = system(command.c_str()); //* FILE SENDING TO EMAIL USING PYTHON CODE
   
           command.clear(); 
           command = AMS_Path + "\\OTHER\\mail.py"; 
@@ -52,10 +53,20 @@ using namespace std;
           remove(command.c_str()); //delete py file
           remove(AttachmentPath.c_str()); //delete attachment file
           
-          return(true);// work successful
+          process_flag=true;
+
+          if(err)
+          email_flag=false;
+          else
+          email_flag=true;
+
       }
-  
-}
+
+     
+      
+  }
+
+
 
 int main()
 {
